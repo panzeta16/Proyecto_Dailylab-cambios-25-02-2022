b9541d01a92079b38591db5c217e8ce32790a317
steven gonzalez <davidhawkins@gmail.com>
2022-02-25 12:00:45 -0500

    cambios del 25/02/2022

---
+++ Proyecto_Dailylab-master/Views/js/sweetAlert.js
@@ -0,0 +1,22 @@
+Swal.fire({
+	title: "Bienvenido",
+    text: "Espero que te guste mi pagina",
+    icon: 'question',
+    confirmButtonText: 'Seleccionar',
+    backdrop: true,
+    allowOutsideClick: true,
+    allowEscapeKey: true,
+    allowEnterKey: true,
+    timer: 5000,
+    timerProgressBar: true,
+    confirmButtonColor: '#0e5cb6',
+    showCancelButton: true,
+    cancelButtonText: 'Cancelar',
+    cancelButtonLabel: 'Cancelar',
+    showCloseButton: true,
+    closeButtonAriaLabel: 'cerrar alerta',
+    /*imageUrl: 'ruta'
+    imageWidth: '200px',
+    imageAlt: 'Camara'*/
+
+});
